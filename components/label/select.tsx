018bc68ffd2fc4028b1ab77442535a9b8d708c5a
karl96k <karl.gustav1789@gmail.com>
2021-11-07 13:20:01 +0200

    lable task

---
+++ components/label/select.tsx
@@ -0,0 +1,37 @@
+import {Select} from "antd";
+
+const {Option} = Select;
+
+interface SelectModelProps {
+    models: string[],
+    dataSets: string[],
+
+    onModelSelected(newModel: string): void,
+
+    onDataSetSelected(newModel: string): void;
+}
+
+export function SelectSection(props: SelectModelProps) {
+    const {models, dataSets, onModelSelected, onDataSetSelected} = props;
+
+    return (
+        <>
+            <Select defaultValue={models[0]} onChange={(value) => onModelSelected(value)}>
+                {
+                    models.map(model => (
+                        <Option key={model} value={model}>{model}</Option>
+                    ))
+                }
+            </Select>
+            <br/>
+            <br/>
+            <Select defaultValue={dataSets[0]} onChange={(value) => onDataSetSelected(value)}>
+                {
+                    dataSets.map(data => (
+                        <Option key={data} value={data}>{data}</Option>
+                    ))
+                }
+            </Select>
+        </>
+    )
+}
