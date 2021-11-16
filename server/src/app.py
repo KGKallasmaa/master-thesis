b71ba9b40ec02f3c183661e76ce85807fbe2e3ec
Karl-Gustav Kallasmaa <karl-gustav.kallasmaa@transferwise.com>
2021-11-16 22:17:17 +0200

    switch to flask

--- server/src/app.py
+++ server/src/app.py
@@ -1,46 +1,49 @@
-from sanic import Sanic
-from sanic.response import json
-
 from main.service.data_access import get_labels
 from main.service.index_segments import image_segments
 from main.service.lable_image import label_example_image, label_all_images
 from flask_cors import CORS
+from flask import jsonify
+from flask import request
+from flask import Flask
 
-app = Sanic("Master thesis service")
+app = Flask(__name__)
+
 CORS(app)
 
 
-
 @app.route("/all-labels", methods=["POST"])
-async def all_labels_view(request):
+async def all_labels_view():
     labels = list(set(get_labels().tolist()))
-    return json({"labels": labels})
+    return jsonify({"labels": labels})
 
 
 @app.route("/label-image", methods=["POST"])
-async def label_image_view(request):
-    label = request.json["label"]
+def label_image_view():
+    payload = request.get_json()
+    label = payload["label"]
     if len(label) == 0:
-        return json({"index": -1, "url": "", "label": label})
+        return jsonify({"index": -1, "url": "", "label": label})
     index, url = label_example_image(label)
-    return json({"index": index, "url": url, "label": label})
+    return jsonify({"index": index, "url": url, "label": label})
 
 
 @app.route("/image-segments", methods=["POST"])
-async def image_segment_view(request):
-    index = request.json["index"]
+def image_segment_view():
+    payload = request.get_json()
+    index = payload["index"]
     if index == -1:
-        return json({"results": []})
+        return jsonify({"results": []})
     results = image_segments(index)
-    return json({"results": results})
+    return jsonify({"results": results})
 
 
 @app.route("/label-all-images", methods=["POST"])
-async def label_all_image_view(request):
-    label = request.json["label"]
+def label_all_image_view():
+    payload = request.get_json()
+    label = payload["label"]
     results = label_all_images(label) if len(label) > 0 else []
-    return json({"results": results})
+    return jsonify({"results": results})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False, access_log=False)
+    app.run(host='0.0.0.0', port=5000)
