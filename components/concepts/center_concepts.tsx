d90e1806aa042aef4f2a2d466fa32ad7b8a161f0
karl96k <karl.gustav1789@gmail.com>
2021-12-15 17:44:43 +0200

    changes

---
+++ components/concepts/center_concepts.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { http } from "../common/http";
+import { Col, Row, Skeleton } from "antd";
+import { ConceptCard } from "../common/card";
+import DetailedConcept from "./detail_concepts";
+
+export default function CenterConcepts(props: { index: number }) {
+    const { index } = props;
+    const [images, setImage] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        http("/center-most-concepts", {})
+            .then((el) => el.json())
+            .then((data) => {
+                setImage(data.results);
+                setIsLoading(false);
+            })
+            .catch(() => {
+                setIsLoading(false);
+            });
+    }, [index]);
+    const [selectedConcepts,setSelectedConcepts] = useState<string[]>([])
+
+    function handleConceptWillBeUsed(name: string, decision: boolean) {
+        if (decision == true){
+            setSelectedConcepts(selectedConcepts.concat(name))
+        }
+    }
+
+    if (isLoading) return <Skeleton active />;
+
+    return (
+        <Row>
+            {images.map((el, i) => (
+                <Col span={8} key={i} style={{ marginRight: 50, marginBottom: 20 }}>
+                    <ConceptCard
+                        key={i}
+                        label={el.conceptName}
+                        imageBase64={el.src}
+                        imageWidth={200}
+                        onSelected={handleConceptWillBeUsed}
+                    />
+                    {selectedConcepts.includes(el.conceptName) && <DetailedConcept name={el.conceptName}/>}
+                </Col>
+            ))}
+        </Row>
+    );
+}
--- server/src/app.py
+++ server/src/app.py
@@ -1,6 +1,11 @@
-from main.service.data_access import get_labels
+from requests import api
+
+from main.service.common import serve_pil_image
+from main.service.data_access import get_labels, get_images
 from main.service.index_segments import image_segments
+from main.service.kmeans import CENTER_MOST_CONCEPTS, CONCEPT_K_REPRESENTATIVES
 from main.service.lable_image import label_example_image, label_all_images
+import numpy as np
 from flask_cors import CORS
 from flask import jsonify
 from flask import request
@@ -15,8 +20,55 @@ CORS(app)
 def health_view():
     return jsonify({"hello": "world"})
 
+# TODO: this is used
+@api.route("/upload-image", methods=["POST"])
+def upload_images_view():
+    image = request.files['file'].read()
+    image_as_ar = np.fromstring(image, np.uint8)
+    index = find_image_index(image_as_ar)
+    return jsonify({"index": index})
 
-@app.route("/all-labels", methods=["POST"])
+
+# TODO: this is used
+@api.route("/image-by-index", methods=["POST"])
+def image_by_index_view():
+    payload = request.get_json()
+    index = payload["index"]
+    if index is None:
+        return jsonify({"url": "", "label": ""})
+    img = get_images()[index]
+    label = get_labels()[index]
+    return jsonify({"url": serve_pil_image(img), "label": label})
+
+
+# TODO: this is used
+@api.route("/center-most-concepts", methods=["POST"])
+def label_concepts_view():
+    return jsonify({"results": CENTER_MOST_CONCEPTS})
+
+
+# TODO: this is used
+@api.route("/concept-representatives", methods=["POST"])
+def concept_representative_view():
+    payload = request.get_json()
+    concept_name = payload["name"]
+    if concept_name is None or concept_name == "":
+        return jsonify({"results": []})
+    results = CONCEPT_K_REPRESENTATIVES.get(concept_name, [])
+    return jsonify({"results": results})
+
+
+@api.route("/image-segments", methods=["POST"])
+def image_segment_view():
+    payload = request.get_json()
+    index = payload["index"]
+    if index == -1:
+        return jsonify({"results": []})
+    results = image_segments(index)
+    return jsonify({"results": results})
+
+
+@api.route("/all-labels", methods=["POST"])
 def all_labels_view():
     labels = list(set(get_labels().tolist()))
     return jsonify({"labels": labels})
