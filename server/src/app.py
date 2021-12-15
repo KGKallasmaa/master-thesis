9a80f4992e5408eb357161ddff8d31c6998f0c86
karl96k <karl.gustav1789@gmail.com>
2021-12-15 18:04:28 +0200

    some things are missing

--- server/src/app.py
+++ server/src/app.py
@@ -8,6 +8,7 @@ from main.service.common import serve_pil_image
 from main.service.data_access import get_labels, get_images
 from main.service.image_index import find_image_index
 from main.service.index_segments import image_segments
+from main.service.kmeans import CENTER_MOST_CONCEPTS, CONCEPT_K_REPRESENTATIVES
 from main.service.kmeans import center_most_concepts
 from main.service.lable_image import label_example_image, label_all_images
 
@@ -82,6 +83,60 @@ def label_all_image_view():
     label = payload["label"]
     results = label_all_images(label) if len(label) > 0 else []
     return jsonify({"results": results})
+
+
+# TODO: this is used
+@api.route("/upload-image", methods=["POST"])
+def upload_images_view():
+    image = request.files['file'].read()
+    image_as_ar = np.fromstring(image, np.uint8)
+    index = find_image_index(image_as_ar)
+    return jsonify({"index": index})
+
+
+# TODO: this is used
+@api.route("/image-by-index", methods=["POST"])
+def image_by_index_view():
+    payload = request.get_json()
+    index = payload["index"]
+    if index is None:
+        return jsonify({"url": "", "label": ""})
+    img = get_images()[index]
+    label = get_labels()[index]
+    return jsonify({"url": serve_pil_image(img), "label": label})
+
+
+# TODO: this is used
+@api.route("/center-most-concepts", methods=["POST"])
+def label_concepts_view():
+    return jsonify({"results": CENTER_MOST_CONCEPTS})
+
+
+# TODO: this is used
+@api.route("/concept-representatives", methods=["POST"])
+def concept_representative_view():
+    payload = request.get_json()
+    concept_name = payload["name"]
+    if concept_name is None or concept_name == "":
+        return jsonify({"results": []})
+    results = CONCEPT_K_REPRESENTATIVES.get(concept_name, [])
+    return jsonify({"results": results})
+
+
+@api.route("/image-segments", methods=["POST"])
+def image_segment_view():
+    payload = request.get_json()
+    index = payload["index"]
+    if index == -1:
+        return jsonify({"results": []})
+    results = image_segments(index)
+    return jsonify({"results": results})
+
+
+@api.route("/all-labels", methods=["POST"])
+def all_labels_view():
+    labels = list(set(get_labels().tolist()))
+    return jsonify({"labels": labels})
 
 
 if __name__ == '__main__':
--- server/src/main/service/kmeans.py
+++ server/src/main/service/kmeans.py
@@ -84,9 +84,123 @@ def center_most_concepts(k=10) -> List[any]:
     return results
 
 
+def center_most_concepts(k=10) -> List[any]:
+    """
+    Every image (e.g. bedroom) is filled with segments (e.g bed, lamp, window).
+    Our task is to give user top k(k=10) segments that best describe the image.
+    """
+    return kmean_segments(get_images()[:300], get_masks()[:300], k)
+
+
+def concept_representatives(k=5) -> Dict[str, List[any]]:
+    """
+    Every image (e.g. bedroom) is filled with segments (e.g bed, lamp, window).
+    Our task is to find k=5 best representatives from the given concept. E.g. we find the k top beds from our dataset
+    """
+
+    all_concepts = [el["conceptName"] for el in CENTER_MOST_CONCEPTS]
+
+    all_results = {}
+    all_images = get_images()
+    all_masks = get_masks()
+
+    for name in all_concepts:
+        training_data = []
+        segment_lookup = {}
+        my_labels = []
+
+        for pic, mask in zip(all_images, all_masks):
+            segss, seg_class = get_segments(np.array(pic), mask, threshold=0.005)
+            segss = [s for index, s in enumerate(segss) if seg_class[index] == name]
+
+            for s in segss:
+                to_img = array_to_image(s)
+                s = np.array(resize_img(to_img)).flatten()
+                segment_lookup[str(s)] = np.array(resize_img(to_img))
+                training_data.append(np.array(s))
+                my_labels.append(name)
+
+        kmeans = KMeans(n_clusters=min(len(training_data), k), random_state=0).fit(training_data)
+
+        all_distances = [euclidean_distance(segment, kmeans.cluster_centers_[label_index]) for label_index, segment in
+                         zip(kmeans.labels_, training_data)]
+        all_distances.sort()
+        smallest_distances = all_distances[:k]
+
+        results = []
+
+        for label_index, segment in zip(kmeans.labels_, training_data):
+            distance = euclidean_distance(segment, kmeans.cluster_centers_[label_index])
+            if distance in smallest_distances:
+                lookup = segment_lookup[str(segment)]
+                segment_as_arr = array_to_image(lookup)
+                results.append({"conceptName": name, "src": serve_pil_image(segment_as_arr)})
+
+        all_results[name] = results
+    return all_results
+
+
+# TODO: rename
+
+def kmean_segments(images, masks, k=10):
+    training_data = []
+
+    segment_lookup = {}
+
+    my_labels = []
+
+    for pic, mask in zip(images, masks):
+
+        segss, seg_class = get_segments(np.array(pic), mask, threshold=0.005)
+
+        for s in segss:
+            to_img = array_to_image(s)
+
+            s = np.array(resize_img(to_img)).flatten()
+
+            segment_lookup[str(s)] = np.array(resize_img(to_img))
+
+            training_data.append(np.array(s))
+
+        my_labels.extend(seg_class)
+
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(training_data)
+
+    label_bestimg = {}
+
+    for label_index, segment in zip(kmeans.labels_, training_data):
+
+        conceptName = my_labels[label_index]
+
+        currentSegment, currentDistance = label_bestimg.get(conceptName, (None, float('inf')))
+
+        distance = euclidean_distance(segment, kmeans.cluster_centers_[label_index])
+
+        if distance < currentDistance:
+
+            lookup = segment_lookup[str(segment)]
+
+            if lookup is None:
+                continue
+
+            segment_as_arr = array_to_image(lookup)
+
+            label_bestimg[conceptName] = (serve_pil_image(segment_as_arr), distance)
+
+    results = []
+
+    for label, (segment, distance) in label_bestimg.items():
+        results.append({"conceptName": label, "src": segment})
+    return results
+
+
 def euclidean_distance(a: np.array, b: np.array) -> float:
     return np.linalg.norm(a - b)
 
 
 def cosine_similarity(a: np.array, b: np.array) -> float:
     return np.dot(a, b) / (np.norm(a) * np.norm(b))
+
+
+CENTER_MOST_CONCEPTS = center_most_concepts()
+CONCEPT_K_REPRESENTATIVES = concept_representatives()
