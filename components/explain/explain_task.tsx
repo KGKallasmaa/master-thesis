7bbae8bb575b2f86bfbec4a8ef276cc28ffbb78c
Karl-Gustav Kallasmaa <karl-gustav.kallasmaa@transferwise.com>
2022-01-21 13:31:41 +0000

    formattttt

--- components/explain/explain_task.tsx
+++ components/explain/explain_task.tsx
@@ -1,105 +1,107 @@
-import {ExplainableHeader} from "../common/header";
-import {CurrentStep, ExplainableSteps} from "../common/steps";
-import {Col, Row} from "antd";
-import {getId} from "../common/storage";
+import { ExplainableHeader } from "../common/header";
+import { CurrentStep, ExplainableSteps } from "../common/steps";
+import { Col, Row } from "antd";
+import { getId } from "../common/storage";
 import CurrentImage from "../concepts/current_image";
-import {useEffect, useState} from "react";
-import {http} from "../common/http";
+import { useEffect, useState } from "react";
+import { http } from "../common/http";
 
 export default function ExplainTask(index: number) {
-    const title = "Explain";
-    const description = "Explaining this image using a decision tree";
-    return (
-        <>
-            <br/>
-            <ExplainableHeader title={title} description={description}/>
-            <br/>
-            <ExplainableSteps step={CurrentStep.ExplainModels}/>
-            <br/>
-            <Row>
-                <Col span={8}/>
-                <Col span={8}>
-                    <CurrentImage index={index}/>
-                    <br/>
-                    <MachineLearningExplanation index={index}/>
-                </Col>
-                <Col span={8}/>
-            </Row>
-        </>
-    );
+  const title = "Explain";
+  const description = "Explaining this image using a decision tree";
+  return (
+    <>
+      <br />
+      <ExplainableHeader title={title} description={description} />
+      <br />
+      <ExplainableSteps step={CurrentStep.ExplainModels} />
+      <br />
+      <Row>
+        <Col span={8} />
+        <Col span={8}>
+          <CurrentImage index={index} />
+          <br />
+          <MachineLearningExplanation index={index} />
+        </Col>
+        <Col span={8} />
+      </Row>
+    </>
+  );
 }
 
 function MachineLearningExplanation(props: { index: number }) {
-    const [isLoading, setLoading] = useState(true);
-    const [errorMessage, setErrorMessage] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-    const [explanations, setExplanations] = useState<string[]>([]);
-    const [plainTreeExplanation, setPlainTreeExplanation] = useState<string[]>([]);
-    const [trueLabel, setTrueLabel] = useState<string>("");
-    const [predictedLabel, setPredictedLabel] = useState<string>("");
+  const [explanations, setExplanations] = useState<string[]>([]);
+  const [plainTreeExplanation, setPlainTreeExplanation] = useState<string[]>(
+    []
+  );
+  const [trueLabel, setTrueLabel] = useState<string>("");
+  const [predictedLabel, setPredictedLabel] = useState<string>("");
 
-    useEffect(() => {
-        const{index} = props
-        if (index == null){
-            setErrorMessage("Image index not found");
-            setLoading(false);
-            return
-        }
-        const payload = {
-            img: index,
-            id: getId(),
-        };
-        http("/explain-using-concepts", payload)
-            .then((el) => el.json())
-            .then((data) => {
-                setTrueLabel(escape(data.trueLabel));
-                setPredictedLabel(escape(data.predictedLabel));
-                setExplanations(data.explanations.map(el=>escape(el)));
-                setPlainTreeExplanation(data.plainTextTree.map(el=>escape(el)));
-                console.log("hii")
-            })
-            .catch((err) => {
-                setErrorMessage(err.message);
-            })
-            .finally(() => {
-                setLoading(false);
-            });
-    }, [props.index]);
-
-    if (isLoading) {
-        return <p>generating an explanation ...</p>;
+  useEffect(() => {
+    const { index } = props;
+    if (index == null) {
+      setErrorMessage("Image index not found");
+      setLoading(false);
+      return;
     }
-    if (errorMessage) {
-        return (
-            <div>
-                <h3>Explanation has failed</h3>
-                <p>{errorMessage}</p>
-            </div>
-        );
-    }
+    const payload = {
+      img: index,
+      id: getId(),
+    };
+    http("/explain-using-concepts", payload)
+      .then((el) => el.json())
+      .then((data) => {
+        setTrueLabel(escape(data.trueLabel));
+        setPredictedLabel(escape(data.predictedLabel));
+        setExplanations(data.explanations.map((el) => escape(el)));
+        setPlainTreeExplanation(data.plainTextTree.map((el) => escape(el)));
+        console.log("hii");
+      })
+      .catch((err) => {
+        setErrorMessage(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [props.index]);
 
+  if (isLoading) {
+    return <p>generating an explanation ...</p>;
+  }
+  if (errorMessage) {
     return (
-        <div>
-            <h3>True label:{trueLabel}</h3>
-            <br/>
-            <h3>Predicted label:{predictedLabel}</h3>
-            <br/>
-            <h3>Path explanations</h3>
-            <br/>
-            {explanations.map((el) => (
-                <div>
-                    <p>{el}</p>
-                    <br/>
-                </div>
-            ))}
-            <h3>Tree representation</h3>
-            <br/>
-            {plainTreeExplanation.map((el) => (
-                <div>
-                    <p>{el}</p>
-                    <br/>
-                </div>
-            ))}
-        </div>
+      <div>
+        <h3>Explanation has failed</h3>
+        <p>{errorMessage}</p>
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <h3>True label:{trueLabel}</h3>
+      <br />
+      <h3>Predicted label:{predictedLabel}</h3>
+      <br />
+      <h3>Path explanations</h3>
+      <br />
+      {explanations.map((el) => (
+        <div>
+          <p>{el}</p>
+          <br />
+        </div>
+      ))}
+      <h3>Tree representation</h3>
+      <br />
+      {plainTreeExplanation.map((el) => (
+        <div>
+          <p>{el}</p>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
 }
