7182fa28cc7802da1bbc0776d154b1731bede9b4
KGKallasmaa <29797627+KGKallasmaa@users.noreply.github.com>
2022-09-25 14:18:35 +0100

    dice

--- server/src/main/service/explain/counterfactual_explanation.py
+++ server/src/main/service/explain/counterfactual_explanation.py
@@ -1,9 +1,11 @@
 import math
 from typing import List
 
-from alibi.explainers import Counterfactual
+import dice_ml
+import pandas as pd
 from pymongo import MongoClient
 from main.database.explanation_requirement import ExplanationRequirementDb
+from main.models.explanation_requirement import ExplanationRequirement
 from main.service.explain.explain import encode_categorical_values, get_training_row, train_decision_tree
 from main.service.explain.human_readable_explanation import HumanReadableExplanationService
 from main.service.pre_explanation.data_access import get_labels, get_images, get_masks
@@ -16,84 +18,65 @@ class CounterFactualExplanationService:
     def __init__(self, client: MongoClient):
         self.explanation_requirement_db = ExplanationRequirementDb(client)
 
-    def counterfactual_explanation(self, explanation_id: str):
-        constrains = self.explanation_requirement_db.get_explanation_requirement(explanation_id)
-        excluded_node_ids = self.__to_be_excluded_node_ids(explanation_id, constrains.counter_factual)
-        feature_encoder = encode_categorical_values(constrains.available_concepts)
-        label_encoder, X, y, decision_tree = self.__regular_explain_tree(explanation_id)
-        """
-        available_concepts = constrains.available_concepts
-        label_encoder, X, y, decision_tree = self.__regular_explain_tree(explanation_id)
-        feature_encoder = encode_categorical_values(available_concepts)
-        excluded_node_ids = self.__to_be_excluded_node_ids(explanation_id, constrains.counter_factual)
-        """
-        print(X,flush=True)
-        shape = (1,len(X[0]))
-        cf = Counterfactual(decision_tree, shape, distance_fn='l1', target_proba=1.0,
-                            target_class=constrains.counter_factual, max_iter=1000, early_stop=50, lam_init=1e-1,
-                            max_lam_steps=10, tol=0.05, learning_rate_init=0.1,
-                            feature_range=(-1e10, 1e10), eps=0.01, init='identity',
-                            decay=True, write_dir=None, debug=True)
-        explanation = cf.explain(X)
+    def counterfactual_explanation(self, explanation_id: str, to_be_explained_image_index: int):
+        explanation_requirement = self.explanation_requirement_db.get_explanation_requirement(explanation_id)
 
-        print("FINALLY WE ARE HERE!!!!!!")
-        print(explanation,flush=True)
-        return explanation
+        label_encoder, X, y, decision_tree = self.__regular_explain_tree(explanation_requirement)
 
-        """
-        hre = HumanReadableExplanationService(label_encoder=label_encoder,
-                                              feature_encoder=feature_encoder,
-                                              estimator=decision_tree)
+        # 1. Initialize dice
+        dice_explanation = dice_ml.Model(model=decision_tree, backend="sklearn")
+        dice_transformed_data = self.__transform_data_for_dice(
+            X, y,
+            explanation_requirement.available_concepts,
+            label_encoder
+        )
+        dice_data = dice_ml.Data(dataframe=dice_transformed_data,
+                                 continuous_features=explanation_requirement.available_concepts,
+                                 outcome_name="label")
 
-        return hre.human_readable_counterfactual_explanation(counterfacutal_label=constrains.counter_factual,
-                                                             excluded_nodes=excluded_node_ids)
-        """
+        # 2. Initialize the explain
+        exp = dice_ml.Dice(dice_data, dice_explanation, method="random")
 
-    def __to_be_excluded_node_ids(self, explanation_id: str, counterfactual_class: str) -> List[int]:
-        """
-        We are excluding some nodes, cause they lead to our target class
-        """
-        label_encoder, X, y, decision_tree = self.__regular_explain_tree(explanation_id)
-        return self.__nodes_leading_to_counterfactual(
-            counterfactual_class,
-            decision_tree,
-            label_encoder,
-            X)
+        # 3. Generate the counterfactual
+        to_be_explained_instance = dice_transformed_data.to_dict(orient='records')[to_be_explained_image_index]
 
-    def __regular_explain_tree(self, explanation_id:str):
-        constrains = self.explanation_requirement_db.get_explanation_requirement(explanation_id)
-        available_concepts = constrains.available_concepts
+        # dice_transformed_data.iloc[to_be_explained_image_index]
+        counterfactual = exp.generate_counterfactuals(
+            to_be_explained_instance,
+            total_CFs=1,
+            desired_class=explanation_requirement.counter_factual
+        )
+        return counterfactual.visualize_as_list()
+
+    @staticmethod
+    def __transform_data_for_dice(X, y, concepts: List[str], label_encoder):
+        data = {}
+        for x_i, y_i in zip(X, y):
+            current_y_values = data.get("label", [])
+            current_y_values.append(label_encoder.inverse_transform([y_i])[0])
+            data["label"] = current_y_values
+
+            for i, c in enumerate(concepts):
+                current_c_values = data.get(c, [])
+                current_c_values.append(x_i[i])
+                data[c] = current_c_values
+        return pd.DataFrame(data=data)
+
+    @staticmethod
+    def __regular_explain_tree(explanation_requirement: ExplanationRequirement):
+        available_concepts = explanation_requirement.available_concepts
         available_concepts.sort()
 
         if len(available_concepts) == 0:
             raise RuntimeError("Explanation can not be provided, because we can not use any concepts")
 
         label_encoder = encode_categorical_values(get_labels())
-        training_data, training_labels = [], []
-        for index, (label, pic, mask) in enumerate(zip(get_labels(), get_images(), get_masks())):
+        X, y = [], []
+        for label, pic, mask in zip(get_labels(), get_images(), get_masks()):
             row = get_training_row(available_concepts, pic, mask)
-            label_as_nr = label_encoder.transform([label])
-            training_labels.append(label_as_nr)
-            training_data.append(row)
+            label_as_nr = label_encoder.transform([label])[0]
+            y.append(label_as_nr)
+            X.append(row)
 
-        clf = train_decision_tree(np.array(training_data), np.array(training_labels))
-        return label_encoder, training_data, training_labels, clf
-
-
-    @staticmethod
-    def __nodes_leading_to_counterfactual(counter_factual_class: str, decision_tree, label_encoder, features) -> List[
-        int]:
-        decision_paths = decision_tree.decision_path(features).toarray()
-        leaf_ids = decision_tree.apply(features)
-        print("unique leaf ids: " + str(list(set(leaf_ids))), flush=True)
-
-        nodes_leading_to_target_class = []
-        for i, decision_path in enumerate(decision_paths):
-            leaf_category = label_encoder.inverse_transform([leaf_ids[i]])[0]
-            is_counterfactual_class = leaf_category == counter_factual_class
-            if is_counterfactual_class:
-                nodes_leading_to_target_class += [i for i, node_value in enumerate(decision_path) if node_value == 1]
-
-        nodes_leading_to_target_class = list(set(nodes_leading_to_target_class))
-        print("nodes leading to target class" + str(list(nodes_leading_to_target_class)), flush=True)
-        return sorted(nodes_leading_to_target_class)
+        clf = train_decision_tree(np.array(X), np.array(y))
+        return label_encoder, X, y, clf
