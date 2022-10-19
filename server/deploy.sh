d6d386fd6f5a441f7edf82f565751803b6c2df3b
Karl-Gustav Kallasmaa <29797627+KGKallasmaa@users.noreply.github.com>
2022-10-20 00:14:06 +0100

    Add missing script changes (#28)
    * fix some bugs
    
    * allow selecting
    
    * more progress
    
    * Update footer.tsx
    
    * current progress
    
    * provide explain
    
    * change deployment script

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,6 +1,6 @@
 docker login --username=rocketnow
 docker-compose build
 docker login docker.io
-docker tag server_master_thesis_server:latest rocketnow/server_master_thesis_server:latest
-docker push rocketnow/server_master_thesis_server:latest
+docker tag master_thesis_server:latest rocketnow/master_thesis_server:latest
+docker push rocketnow/master_thesis_server:latest
 docker system prune -a
