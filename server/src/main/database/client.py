fe31ebd1c4a31cfafdc5169aea98d6bb31eb574b
Karl-Gustav Kallasmaa <29797627+KGKallasmaa@users.noreply.github.com>
2022-11-22 22:55:46 +0000

    local db

--- server/src/main/database/client.py
+++ server/src/main/database/client.py
@@ -1,10 +1,6 @@
 import os
 import certifi
-from dotenv import load_dotenv
 from pymongo import MongoClient
-from pymongo.server_api import ServerApi
-
-load_dotenv()
 
 ca = certifi.where()
 
@@ -12,4 +8,8 @@ ca = certifi.where()
 def get_client() -> MongoClient:
     """Returns the MongoDB client"""
     url = os.getenv("DB_URL")
-    return MongoClient(url, server_api=ServerApi('1'), tlsCAFile=ca)
+    try:
+        client = MongoClient(url)
+    except:
+        raise ConnectionError("Could not connect to MongoDB")
+    return client
